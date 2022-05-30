3ed903516822998359cd80dfeaa74aaf7f6e46ad
kzelenovic <106201960+kzelenovic@users.noreply.github.com>
2022-05-30 14:30:36 -0700

    Added quotes
    added quotes and getRandomQuote function

--- js/script.js
+++ js/script.js
@@ -11,19 +11,56 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source: 'Nelson Mandela',
+    citation: 'Long Walk to Freedom',
+    year: 1995
+  },
+  {
+    quote: 'I only hope that we never lose sight of one thing — that it all started with a mouse.',
+    source: 'Walt Disney',
+    citation: 'What is Disneyland',
+    year: 1954
+  },
+  {
+    quote: 'If life were predictable it would cease to be life, and be without flavor.',
+    source: 'Eleanor Roosevelt',
+    citation: 'Tomorrow Is Now',
+    year: 1963
+  },
+  {
+    quote: 'I\'m still off hamburgers.',
+    source: 'Oprah Winfrey',
+    citation: 'Oprah: \'Free speech rocks\'',
+    year: 1998
+  },
+  {
+    quote: 'Imagination is a force that can actually manifest a reality.',
+    source: 'James Cameron',
+    citation: 'TED conference',
+    year: 2010
+  }
+];
 
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length )
+  console.log(randomNumber);
+  return arr[randomNumber];
+}
 
+console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function
 ***/
-
-
 
 /***
  * click event listener for the print quote button
